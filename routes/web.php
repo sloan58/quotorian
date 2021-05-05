3c55685c5e0105d5818c5d5f0c564bbf186a2963
Marty Sloan <marty@karmatek.io>
2021-05-05 17:36:20 -0400

    clean up home page

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index' ])->name('home');
 
 Route::group(['middleware' => 'auth'], function () {
 	Route::resource('user', App\Http\Controllers\UserController::class)->except(['show']);
