03351e809ed7da3958b1977b1c58ef9f6960f6e2
Marty Sloan <marty@karmatek.io>
2021-05-18 13:43:57 -0400

    wip: book edit ui

--- resources/views/livewire/edit-book.blade.php
+++ resources/views/livewire/edit-book.blade.php
@@ -1,46 +1,60 @@
 <div class="content">
-    <div class="container">
+    <div class="container ml-0">
         <div class="row justify-content-start">
-            <div class="col-lg-12">
+            <div class="col-lg-6">
                 <div class="card shadow">
                     <div class="card-body">
-                        <div class="row d-flex">
-                            <div class="col-2 h-100" style="min-height: 192px; min-width: 128px;">
+                        <div class="row d-flex justify-content-start align-items-center mx-3">
+                            <div>
                                 <img
                                     src="{{ $book->thumbnail }}"
                                     class="img-responsive"
                                     style="min-height: 192px; min-width: 128px;"
                                 >
                             </div>
+                            <div class="ml-5">
+                                <div class="row">
+                                    <p class="font-weight-bold">{{ $book->title }}</p>
+                                </div>
+                                <div class="row">
+                                    <ul class="list-unstyled">
+                                        <li>Author: {{ $book->author }}</li>
+                                        <li>Published: {{ $book->publishedDate }}</li>
+                                        <li>Pages: {{ $book->pageCount }}</li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <hr>
+                        <div class="row d-flex justify-content-center">
+                            <h5>Book Description</h5>
+                        </div>
+                        <div class="row d-flex justify-content-center">
                             <div class="col-10 mt-2">
-                                <p class="font-weight-bold">{{ $book->title }}</p>
                                 <p>{{ $book->description}}</p>
-                                <ul class="list-unstyled">
-                                    <li>Author: {{ $book->author }}</li>
-                                    <li>Published: {{ $book->publishedDate }}</li>
-                                    <li>Pages: {{ $book->pageCount }}</li>
-                                </ul>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-        <div class="row justify-content-center">
-            <div class="form-group">
-                <textarea class="form-control" id="exampleFormControlTextarea1" rows="300" cols="50"></textarea>
-                <small class="form-text text-muted text-center">Quote Text</small>
-            </div>
-        </div>
-        <div class="row justify-content-center">
-            <div class="form-group">
-                <input class="form-control text-center" placeholder="On Page">
-                <small class="form-text text-muted text-center">(optional)</small>
-            </div>
-        </div>
-        <div class="row justify-content-center">
-            <div class="col-4">
-                <button class="btn btn-block btn-success">Add Quote</button>
+            <div class="col-lg-6">
+                <div class="row justify-content-center">
+                    <div class="form-group">
+                        <textarea class="form-control" id="exampleFormControlTextarea1" rows="300" cols="50"></textarea>
+                        <small class="form-text text-muted text-center">Quote Text</small>
+                    </div>
+                </div>
+                <div class="row justify-content-center">
+                    <div class="form-group">
+                        <input class="form-control text-center" placeholder="On Page">
+                        <small class="form-text text-muted text-center">(optional)</small>
+                    </div>
+                </div>
+                <div class="row justify-content-center">
+                    <div class="col-4">
+                        <button class="btn btn-block btn-success">Add Quote</button>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
